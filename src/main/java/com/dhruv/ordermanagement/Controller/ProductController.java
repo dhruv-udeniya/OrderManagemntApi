ef6b3b05d28d3990642fa105ef36d9c0e13f9144
dhruv-udeniya <175002254+dhruv-udeniya@users.noreply.github.com>
2026-09-13 13:03:56 +0530

    Add basic product GET endpoint

---
+++ src/main/java/com/dhruv/ordermanagement/Controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.dhruv.ordermanagement.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController{
+
+
+    @GetMapping
+    public String getProducts(){
+        return "All Products";
+    }
+
+}
