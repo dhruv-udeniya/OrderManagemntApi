d0b0f8719379a69a81d9fec07b2e17b33ddfdb60
dhruv-udeniya <175002254+dhruv-udeniya@users.noreply.github.com>
2026-09-18 14:32:28 +0530

    Add Http Status Code for Product

--- src/main/java/com/dhruv/ordermanagement/Controller/ProductController.java
+++ src/main/java/com/dhruv/ordermanagement/Controller/ProductController.java
@@ -4,6 +4,8 @@ import com.dhruv.ordermanagement.Entity.Product;
 import com.dhruv.ordermanagement.Repository.ProductRepository;
 import com.dhruv.ordermanagement.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,42 +23,42 @@ public class ProductController{
     private ProductService service;
 
     @GetMapping
-    public List<Product> getAllProducts(){
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return ResponseEntity.ok(service.getAllProducts());
     }
 
 
     @PostMapping
-    public Product addProduct(@RequestBody Product product){
-        return service.addProduct(product);
+    public ResponseEntity<Product> addProduct(@RequestBody Product product){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.addProduct(product));
     }
 
 
     @GetMapping("/{id}")
-    public Product getProductById(@PathVariable long id){
-        return service.getProductById(id);
+    public ResponseEntity<Product> getProductById(@PathVariable long id){
+        return ResponseEntity.ok(service.getProductById(id));
     }
 
 
     @GetMapping("/search")
-    public Product getProduct(@RequestParam String name){
-        return service.getProductByName(name);
+    public ResponseEntity<Product> getProduct(@RequestParam String name){
+        return ResponseEntity.ok(service.getProductByName(name));
     }
 
     @GetMapping("/category")
-    public List<Product> getProductByCategory(@RequestParam String category){
-        return service.getProductByCategory(category);
+    public ResponseEntity<List<Product>> getProductByCategory(@RequestParam String category){
+        return ResponseEntity.ok(service.getProductByCategory(category));
     }
 
 
     @DeleteMapping("/{id}")
-    public String deleteProductById(@PathVariable long id){
+    public ResponseEntity<String> deleteProductById(@PathVariable long id){
         service.deleteProducts(id);
-        return "Product Delete Successfully";
+        return ResponseEntity.notFound().build();
     }
 
     @PutMapping("/{id}")
-    public Product updateProducts(@PathVariable long id,@RequestBody Product product){
-        return service.updateProduct(id,product);
+    public ResponseEntity<Product> updateProducts(@PathVariable long id,@RequestBody Product product){
+        return ResponseEntity.ok(service.updateProduct(id,product));
     }
 }
