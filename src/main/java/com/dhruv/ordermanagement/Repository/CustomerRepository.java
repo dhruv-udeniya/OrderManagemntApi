3b5f4979079b8b1359a1d7d37005edce12a67c2a
dhruv-udeniya <175002254+dhruv-udeniya@users.noreply.github.com>
2026-09-18 14:46:08 +0530

    Add Customer Repository File For Customer Data

--- src/main/java/com/dhruv/ordermanagement/Repository/CustomerRepository.java
+++ src/main/java/com/dhruv/ordermanagement/Repository/CustomerRepository.java
@@ -1,2 +1,10 @@
-package com.dhruv.ordermanagement.Repository;public interface CustomerRepository {
+package com.dhruv.ordermanagement.Repository;
+
+import com.dhruv.ordermanagement.Entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+
+
 }
