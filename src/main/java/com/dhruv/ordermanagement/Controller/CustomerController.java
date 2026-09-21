42f3837d99672135e1a64e029ccafb2bbfb1e671
dhruv-udeniya <175002254+dhruv-udeniya@users.noreply.github.com>
2026-09-21 13:27:50 +0530

    Add Http Status Code For Customer RestApi's.

--- src/main/java/com/dhruv/ordermanagement/Controller/CustomerController.java
+++ src/main/java/com/dhruv/ordermanagement/Controller/CustomerController.java
@@ -4,6 +4,8 @@ package com.dhruv.ordermanagement.Controller;
 import com.dhruv.ordermanagement.Entity.Customer;
 import com.dhruv.ordermanagement.Service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,36 +20,37 @@ public class CustomerController {
 
 
     @GetMapping
-    public List<Customer> getAllCustomers(){
-        return service.getAllCustomers();
+    public ResponseEntity<List<Customer>> getAllCustomers(){
+        return ResponseEntity.ok(service.getAllCustomers());
     }
 
     @GetMapping("/{id}")
-    public Customer getCustomerById(@PathVariable long id){
-        return service.customerGetById(id);
+    public ResponseEntity<Customer> getCustomerById(@PathVariable long id){
+        return ResponseEntity.ok(service.customerGetById(id));
     }
 
 
     @GetMapping("/search")
-    public Customer getCustomerByName(@RequestParam String name){
-        return service.customerGetByName(name);
+    public ResponseEntity<Customer> getCustomerByName(@RequestParam String name){
+        return ResponseEntity.ok(service.customerGetByName(name));
     }
 
 
     @PostMapping
-    public Customer addCustomers(@RequestBody Customer customer){
-        return service.addCustomers(customer);
+    public ResponseEntity<Customer> addCustomers(@RequestBody Customer customer) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.addCustomers(customer));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCustomerById(@PathVariable long id){
+    public ResponseEntity<String> deleteCustomerById(@PathVariable long id){
         service.deleteCustomerById(id);
+        return ResponseEntity.notFound().build();
     }
 
 
     @PutMapping("/{id}")
-    public Customer updateCustomer(@PathVariable long id,@RequestBody Customer customer){
-        return  service.updateCustomerById(id,customer);
+    public ResponseEntity<Customer> updateCustomer(@PathVariable long id,@RequestBody Customer customer){
+        return  ResponseEntity.ok(service.updateCustomerById(id,customer));
     }
 
 }
